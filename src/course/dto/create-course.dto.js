eb920469710a0d215681cea8162744d471634130
Nasirin <92769271+kangsunat@users.noreply.github.com>
2026-03-16 23:11:07 +0700

    clear

--- src/course/dto/create-course.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreateCourseDto = void 0;
-var CreateCourseDto = /** @class */ (function () {
-    function CreateCourseDto() {
-    }
-    return CreateCourseDto;
-}());
-exports.CreateCourseDto = CreateCourseDto;
--- src/course/entities/course.entity.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Course = void 0;
-var Course = /** @class */ (function () {
-    function Course() {
-    }
-    return Course;
-}());
-exports.Course = Course;
