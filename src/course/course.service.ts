1e6b243757edb3bb4166b61a651a3f51e167e06b
Nasirin <92769271+kangsunat@users.noreply.github.com>
2026-03-17 00:15:14 +0700

    caching

--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -4,6 +4,7 @@ import { Course } from './course.schema';
 import { Model } from 'mongoose';
 import { CreateCourseDto } from './course.dto';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import type { Cache } from 'cache-manager';
 
 @Injectable()
 export class CourseService {
@@ -13,19 +14,49 @@ export class CourseService {
   ) {}
 
   async create(create: CreateCourseDto): Promise<Course> {
-    const res = new this.courseModel(create);
-    return res.save();
+    const newCourse = await this.courseModel.create(create);
+    await this.cacheManager.clear();
+    return newCourse;
   }
 
   async findAll(page: number = 1, limit: number = 10): Promise<any> {
+    // 1. Buat Cache Key yang unik berdasarkan page dan limit
+    const cacheKey = `courses_p${page}_l${limit}`;
+
+    // 2. Cek Cache
+    const cachedData = await this.cacheManager.get(cacheKey);
+    if (cachedData) {
+      console.log(`Mengambil dari Cache: ${cacheKey}`);
+      return cachedData;
+    }
+
     const skip = (page - 1) * limit;
-    // ambil data dan total hitung secara paralel untuk performa
+
+    // 3. Ambil data dari MongoDB secara paralel
     const [data, total] = await Promise.all([
-      this.courseModel.find().skip(skip).limit(limit).exec(),
+      this.courseModel
+        .find()
+        .sort({ createdAt: -1 })
+        .skip(skip)
+        .limit(limit)
+        .exec(),
       this.courseModel.countDocuments().exec(),
     ]);
 
-    return { data, total };
+    const result = {
+      data,
+      meta: {
+        total,
+        page,
+        limit,
+        lastPage: Math.ceil(total / limit),
+      },
+    };
+
+    // 4. Simpan ke Cache
+    await this.cacheManager.set(cacheKey, result);
+
+    return result;
   }
 
   async addLesson(courseId: string, lessonData: any) {
